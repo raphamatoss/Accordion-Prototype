c20ac162a8a06a923e12296a06c2215b911c6988
Raphael Matos <raphaelmsgoncalves@gmail.com>
2024-01-02 23:29:00 -0300

    Add first content

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const changesIcon = (htmlId) => {
+    
+    document.getElementById(htmlId).style.background = 'url(./assets/images/icon-minus.svg)'
+
+    //title.style.background = 'url(./assets/images/icon-minus.svg)'
+}
