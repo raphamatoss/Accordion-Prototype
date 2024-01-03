b001d8aef61f2b624b0021ef2449b5adb8790855
Raphael Matos <raphaelmsgoncalves@gmail.com>
2024-01-03 17:17:53 -0300

    Basic accordion built

--- javascript.js
+++ javascript.js
@@ -1,6 +1,18 @@
-const changesIcon = (htmlId) => {
-    
-    document.getElementById(htmlId).style.background = 'url(./assets/images/icon-minus.svg)'
+const OpensAccordion = (element1) => (element2) => {
+    const backgroundIcon =  document.getElementById(element1)
+    const content = document.getElementById(element2)
 
-    //title.style.background = 'url(./assets/images/icon-minus.svg)'
+    if (content.style.height != 'auto') {
+        backgroundIcon.style.background = 'url(./assets/images/icon-minus.svg) no-repeat'
+        backgroundIcon.style.backgroundPositionY = 'center'
+        backgroundIcon.style.backgroundPositionX = '95%'
+
+        content.style.height = 'auto'
+    } else {
+        backgroundIcon.style.background = 'url(./assets/images/icon-plus.svg) no-repeat'
+        backgroundIcon.style.backgroundPositionY = 'center'
+        backgroundIcon.style.backgroundPositionX = '95%'
+        
+        content.style.height = 0
+    }
 }
